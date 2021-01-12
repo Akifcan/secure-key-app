fb37daaa75b8a21d710643187ceea733fefce044
Akifcan <48747986+Akifcan@users.noreply.github.com>
2021-01-12 16:14:55 +0300

    app model

---
+++ server/features/apps/AppModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const AppSchema = mongoose.Schema({
+    userId: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        maxLength: 50
+    },
+    apiKey: {
+        type: String,
+        required: true,
+        maxLength: 150
+    },
+    blackList: [],
+    whiteList: [],
+    description: {
+        type: String,
+        maxLength: 500
+    }
+})
+
+export default mongoose.model('apps', AppSchema)
